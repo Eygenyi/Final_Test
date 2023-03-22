c23f376af12cd3ddc3d0dbca28c48a1dc79e4cd8
Evgeniy <freemen0083@gmail.com>
2023-03-22 22:50:15 +0400

    Programm

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string[] arr =
+{
+    "How",
+    "Do",
+    "You",
+    "Do?;)"
+};
+string[] temp = new string[arr.Length];
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i]);
+    if (arr[i].Length <= 3)
+    {
+        temp[i] = arr[i];
+        Console.WriteLine(" --->   " + temp[i]);
+    }
+    else
+    {
+        Console.WriteLine(" --->  >3 symbols");
+    }
+
+}
